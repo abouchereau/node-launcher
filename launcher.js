143fa50bec265187a6fb8331782694a8eac69dcd
anto <anthonybouchereau@hotmail.com>
2021-12-02 21:54:55 +0100

    launcher

--- launcher.js
+++ launcher.js
@@ -13,29 +13,45 @@ const F1 = 65470,
     F5 = 65474,
     F6 = 65475;
 
-let controls = [
+
+process.stdout.write(clc.bold("dO_ob Tony-b Machine"));
+console.log("");
+console.log("");
+process.stdout.write(clc.columns([
     ["F1","volca-drum-hid-control"],
     ["F2","record-audio"],
-] ;
-process.stdout.write(clc.bold("dO_ob"));
+]));
 console.log("");
-process.stdout.write(clc.columns(controls));
 
 let am = new AppManager();
 let em = new EventManager();
 
 ioHook.on('keypress', function (msg) {
+
     if (msg.rawcode == 99 && msg.ctrlKey) {//Ctrl + C
         console.log("EXIT");
         ioHook.unload();
         process.exit();
     }
+
+    if ([F1,F2,F3,F4,F5,F6].includes(msg.rawcode)) {
+        em.ld.stopTurnOnDemo();
+    }
+
     if (msg.rawcode == F1) {
         am.startVolcaDrumHidControl((m)=> {
             em.onVolcaDrumHidControlMessage(m);
         });
     }
+
+    if (msg.rawcode == F2) {
+        am.startStopRecordAudio((m) => {
+            em.onStartStopRecordAudioMessage(m);
+        });
+    }
+
 });
+
 ioHook.start();
 process.stdin.setRawMode(true);
 

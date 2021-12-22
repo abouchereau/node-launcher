65e23fcc435f7b062bb247e40c6a74ea73585044
anto <anthonybouchereau@hotmail.com>
2021-12-22 20:27:05 +0100

    avec node-hid

--- launcher.js
+++ launcher.js
@@ -6,7 +6,8 @@ const AppManager = require("./lib/AppManager");
 const EventManager = require("./lib/EventManager");
 const clc = require('cli-color');
 
-const F1 = 58,
+const C = 6,
+    F1 = 58,
     F2 = 59,
     F3 = 60,
     F4 = 61,
@@ -16,50 +17,48 @@ const F1 = 58,
     F8 = 65;
 
 
-
-let devices = HID.devices();
-
-let vendorId = null;
-let productId = null;
-
-for(let device of devices) {
-    if (device["product"].indexOf("Keyboard")>-1 ||  device["product"].indexOf("2.4G")>-1) {
-        vendorId = device["vendorId"];
-        productId = device["productId"];
-    }
-}
-console.log(vendorId, productId);
-if (vendorId == null) {
-    console.log("No Keyboard detected");
-    process.exit();
-}
-
-let device = new HID.HID(vendorId,productId);
-
-
 process.stdout.write(clc.bold("dO_ob Tony-b Machine"));
 console.log("");
 console.log("");
+
+let devices = HID.devices();
+
+let keyboardDevice = null;
+
+for(let device of devices) {
+    if (device["product"].indexOf("Keyboard")>-1 ||  device["product"].indexOf("2.4G")>-1) {
+        keyboardDevice = device;
+    }
+}
+
+if (keyboardDevice == null) {
+    console.log("No Keyboard detected");
+    process.exit();
+}
+else {
+    console.log("Using Keyboard : "+keyboardDevice.product)
+}
+
+let device = new HID.HID(keyboardDevice.vendorId,keyboardDevice.productId);
 process.stdout.write(clc.columns([
     ["F1","volca-drum-hid-control"],
     ["F2","record-audio"],
     ["F3","tbm-output-audio-level"],
+    ["F8","stop leds"],
 ]));
 console.log("");
 
 let am = new AppManager();
 let em = new EventManager();
-
+let ctrl = false;
 device.on('data',(a)=>{
     let tab = Array.prototype.slice.call(a);
+    ctrl = tab[0] == 1;
     let key = tab[2];
-    console.log("DATA",tab[2]);
-
-   /* if (msg.rawcode == 99 && msg.ctrlKey) {//Ctrl + C
-        console.log("EXIT");
-        ioHook.unload();
+    if (ctrl && key==C) {
+        em.ld.stopTurnOnDemo(true);
         process.exit();
-    }*/
+    }
 
     if ([F1,F2,F3,F4,F5,F6].includes(key)) {
         em.ld.stopTurnOnDemo();
@@ -87,33 +86,4 @@ device.on('data',(a)=>{
     }
 
 });
-/*
-ioHook.start();
 process.stdin.setRawMode(true);
-
-*/
-/*
-const HID = require('node-hid');
-var devices = HID.devices();
-
-let vendorId = null;
-let productId = null;
-
-for(let device of devices) {
-	if (device["product"].indexOf("Keyboard")>-1) {
-		vendorId = device["vendorId"];
-		productId = device["productId"];
-	}
-}
-console.log(vendorId, productId);
-if (vendorId != null) {
-	let device = new HID.HID(vendorId,productId);
-	device.on('data',(a)=>{
-		let tab = Array.prototype.slice.call(a);
-		console.log("DATA",tab[2]);
-	});
-}
-
-
-
- */
